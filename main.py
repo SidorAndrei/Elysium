f7629a14cc1b99a4adefdab27e622262bc6e4bf6
SergiuPert <sergiupert@gmail.com>
2022-04-15 17:51:40 +0300

    background pictures updated, createrd queries file and started the supermarkets page

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 from dotenv import load_dotenv
 
+import queries
+
 load_dotenv()
 app = Flask("Elysium")
 
@@ -8,6 +10,17 @@ app = Flask("Elysium")
 @app.route("/")
 def main_page():
     return render_template('base_template.html')
+
+
+@app.route("/supermarkets")
+def supermarkets_page():
+    supermarkets = queries.get_all_supermarkets()
+    return render_template("supermarkets_page.html", supermarkets=supermarkets)
+
+
+@app.route("/supermarket/<supermarket_id>")
+def supermarket_page(supermarket_id):
+    products = queries.get_products_by_supermarket_id(supermarket_id)
 
 
 def main():
---
+++ queries.py
@@ -0,0 +1,16 @@
+import connection
+
+
+def get_all_supermarkets():
+    return connection.execute_select("""
+    SELECT *
+    FROM supermarket
+    """)
+
+
+def get_products_by_supermarket_id(supermarket_id):
+    return connection.execute_select("""
+    SELECT *
+    FROM products
+    WHERE supermarket_id = %(supermarket_id)s
+    """, {'supermarket_id': supermarket_id})
