755a286699476d064fae1061f28ea2ae0e0173b4
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-04-16 11:30:18 +0300

    map design

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_address():
     return {"address": address}
 
 
-@app.route("/test")
+@app.route("/harta-magazine")
 def test():
     if not 'name' in session:
         return redirect(url_for('login_page'))
