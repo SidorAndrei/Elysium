31c6c7884a7c61caca17343ebf5199550b6f24a9
SergiuPert <sergiupert@gmail.com>
2022-04-16 11:23:13 +0300

    updated queries

--- queries.py
+++ queries.py
@@ -89,3 +89,55 @@ def reject_register_request(request_id):
     """, {"request_id": request_id})
     return user
 
+
+def get_supermarket_by_id(supermarket_id):
+    return connection.execute_select("""
+    SELECT *
+    FROM supermarket
+    WHERE supermarket_id = %(supermarket_id)s
+    """, {"supermarket_id": supermarket_id}, False)
+
+
+def delete_product(product_id):
+    return connection.execute_select("""
+    DELETE 
+    FROM products
+    WHERE product_id = %(product_id)s
+    """, {"product_id": product_id})
+
+
+def update_product(product_id, new_quantity):
+    return connection.execute_select("""
+    UPDATE products 
+    SET quantity = %(new_quantity)s
+    WHERE product_id=%(product_id)s
+    """, {"product_id": product_id, "quantity": new_quantity})
+
+
+def add_products_to_cart_by_id(user_id, product_id, quantity):
+    return connection.execute_dml_statement("""
+        INSERT INTO cart_products (cart_id, product_id, quantity)
+        VALUES ((SELECT cart_id FROM user_cart WHERE user_id = %(user_id)s 
+        ORDER BY cart_id DESC LIMIT 1), %(product_id)s, %(quantity)s)
+        RETURNING 1 as row
+    """, {"user_id": user_id, "product_id": product_id, "quantity": quantity})
+
+
+def get_total_quantity_reserved_by_product_id(product_id):
+    return connection.execute_select("""
+    SELECT SUM(quantity) AS total_quantity
+    FROM cart_products    
+    WHERE product_id = %(product_id)s
+    """, {"product_id": product_id}, False)
+
+
+def get_quantity_by_product_id(product_id):
+    return connection.execute_select("""
+    SELECT quantity
+    FROM products
+    WHERE product_id = %(product_id)s
+    """, {"product_id": product_id}, False)
+
+
+
+# aaaaaaa
